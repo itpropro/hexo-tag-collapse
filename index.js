fec4121291d4218d314b381285a259434bdd2c48
Jan-Henrik Damaschke <j.damaschke@live.de>
2019-09-19 14:24:48 +0200

    Changed collapsible to use html5 details and summary tags

--- index.js
+++ index.js
@@ -4,66 +4,39 @@ hexo.extend.tag.register('collapse', collapse, {ends: true});
 
 function collapse(args, content) {
   var title = args[0];
-  return `<style>
-          .collapsible {
-            background-color: #777;
-            color: white;
-            cursor: pointer;
-            padding: 20px;
-            width: 100%;
-            border: none;
-            text-align: left;
-            outline: none;
-            font-size: 15px;
-          }
-          
-          .active, .collapsible:hover {
-            background-color: #555;
-          }
-          
-          .collapsible:after {
-            content: '\\002B';
-            color: white;
-            font-weight: bold;
-            float: right;
-            margin-left: 5px;
-          }
-          
-          .active:after {
-            content: "\\2212";
-          }
-          
-          .content {
-            padding: 0 18px;
-            max-height: 0;
-            overflow: hidden;
-            transition: max-height 0.2s ease-out;
-            background-color: #f1f1f1;
-          }
-          
-          .collapsetext {
-            margin: 0 0 0 0 !important;
-          }
-          
-          </style>
-          <button class="collapsible">${title}</button>
-          <div class="content">
-            <p>${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}</p>
-          </div>
-          <script>
-          var coll = document.getElementsByClassName("collapsible");
-          var i;
+  var style = `
+  <style>
+  summary {
+    background-color: #777;
+    color: white;
+    padding: 10px;
+    width: 100%;
+    text-align: left;
+    padding: 10px 10px;
+  }
+  details {
+    background-color: #f1f1f1;
+  }
+  summary::-webkit-details-marker {
+    display: none;
+  }
+  summary:after {
+    content: "+";
+    float: left;
+    font-size: 1.5em;
+    font-weight: bold;
+    text-align: center;
+    margin: -5px 10px 0 0;
+  }
+  details[open] summary:after {
+    content: "\\2212";
+  }
+  </style>
+  `
 
-          for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-              this.classList.toggle("active");
-              var content = this.nextElementSibling;
-              if (content.style.maxHeight){
-                content.style.maxHeight = null;
-              } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-              } 
-            });
-          }
-          </script>`;
+  return `${style}
+          <details>
+            <summary>${title}</summary>
+            <p style="padding: 10px">${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}</p>
+          </details>`;
 }
